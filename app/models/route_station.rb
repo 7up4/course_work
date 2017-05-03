ec55859cab7a7af83ffdda14324fdf6bdd26cf4b
Nurlan Musayev <nurlancik1@gmail.com>
2017-05-03 13:05:24 +0300

    Валидации для модели route_station

--- app/models/route_station.rb
+++ app/models/route_station.rb
@@ -2,5 +2,6 @@ class RouteStation < ActiveRecord::Base
   belongs_to :station
   belongs_to :route
 
+  validates :station, :route, presence: true
   validates :station, uniqueness: {scope: :route, message: "one station per route" }
 end

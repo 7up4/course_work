6cddc62e69e88daa217c4724924af035f6ea009b
Nurlan Musayev <nurlancik1@gmail.com>
2017-05-10 17:38:40 +0300

    Понятное представление дней следования

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -2,4 +2,15 @@ module RoutesHelper
   def station_name(station)
     station.present? ? station.name : "Не указана"
   end
+  
+  def get_days(route)
+    days = {"пн.": route.mon, "вт.": route.tues, "ср.": route.wed, "чт.": route.thurs, "пт.": route.fri, "сб.": route.sat, "вс.": route.sun}
+    if days.values.count(true) == days.size
+      "Ежедневно"
+    elsif days.values[0,5].count(true) == 5
+      "По будням"
+    else
+      days.select{|key, value| key if value}.keys.join(", ")
+    end
+  end
 end

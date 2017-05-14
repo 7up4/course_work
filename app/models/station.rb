3af976bd67a95a824600cc30b39e4c49229e4bee
Nurlan Musayev <nurlancik1@gmail.com>
2017-05-14 22:47:35 +0300

    Доработано удаление тарифной зоны

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,6 +4,7 @@ class Station < ActiveRecord::Base
   belongs_to :tariff_zone
   before_destroy :any_route?
   after_save :destroy_tariff_zone
+  before_validation :avoid_uniqueness_fail
   
   has_many :route_starts, class_name: "Station", foreign_key: "start_station_id"
   has_many :route_ends, class_name: "Station", foreign_key: "end_station_id"
@@ -13,15 +14,33 @@ class Station < ActiveRecord::Base
   accepts_nested_attributes_for :tariff_zone, allow_destroy: true, reject_if: :all_blank
 
   validates :name, :number, :tariff_zone, presence: true
-  validates :name, length: {maximum: 64}
+  validates :name, length: {maximum: 64}, uniqueness: true
   validates :number, numericality: { greater_than: 0 }, uniqueness: true
   
   protected
   
+  def avoid_uniqueness_fail
+    if remove_tariff_zone.present?
+      tz_to_remove = TariffZone.find(remove_tariff_zone)
+      if tz_to_remove.present?
+        tmp_name = random_string
+        tz_to_remove.update_attributes(name: tmp_name)
+      end
+    end
+  end
+  
+  def random_string
+    alphabet = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+    loop do
+      string = (0...50).map{ alphabet[rand(alphabet.length)] }.join
+      (return string) if !TariffZone.all.map{|x| x.name}.include? string
+    end
+  end
+  
   def destroy_tariff_zone
     if remove_tariff_zone.present?
       relationship = TariffZone.find(remove_tariff_zone).stations.select{|s| s.id!=self.id}
-      TariffZone.destroy(remove_tariff_zone) if relationship.empty?
+      TariffZone.delete(remove_tariff_zone) if relationship.empty?
     end
   end
   

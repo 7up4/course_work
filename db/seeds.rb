8ac1bc1b5d6970ee2e301cf503c34cf26092087f
Nurlan Musayev <nurlancik1@gmail.com>
2017-05-27 17:47:29 +0300

    Результаты поиска в адаптивном списке. Создание маршрутов в seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,49 @@ RouteStation.delete_all
 Route.delete_all
 Station.delete_all
 TariffZone.delete_all
-5.times{|i| TariffZone.create!(name: "Tariff zone №" + (i+1).to_s)}
-10.times do |i|
+
+tariff_zone_num=5
+station_num=10
+route_num=10
+
+tariff_zone_num.times{|i| TariffZone.create!(name: "Tariff zone №" + (i+1).to_s)}
+
+station_num.times do |i|
   Station.create!(
     name: "Station №"+(i+1).to_s,
-    tariff_zone: TariffZone.all[(0...5).to_a.shuffle[0]],
+    tariff_zone: TariffZone.all[(0...tariff_zone_num).to_a.shuffle[0]],
     number: i+1
   )
+end
+
+def random_hour
+  (Date.today + rand(0..23).hour + rand(0..60).minutes).to_datetime
+end
+
+def random_bool
+  [true, false].sample
+end
+
+route_num.times do |i|
+  r=Route.create(
+    mon: random_bool,
+    tues: random_bool,
+    wed: random_bool,
+    thurs: random_bool,
+    fri: random_bool,
+    sat: random_bool,
+    sun: random_bool
+  )
+  s = []
+  station_num.times{s<<Station.all[(0...station_num).to_a.shuffle[0]]}
+  s.uniq!
+  r.stations<<s
+  r.route_stations.each do |rs|
+    rs.arrival_time = random_hour
+    rs.is_missed = random_bool
+  end
+  rs = r.route_stations.select{|k| !k.is_missed}.sort_by {|x| x[:arrival_time]}
+  r.start_station_id=rs.first.station_id
+  r.end_station_id=rs.last.station_id
+  r.save
 end
